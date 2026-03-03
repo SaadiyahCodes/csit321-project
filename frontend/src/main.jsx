cf8551420ff5eecc85a2ea5d77b3be831a482c5a
Saadiyah <saadiyahasif6@gmail.com>
2026-03-03 14:16:39 +0400

    separate admin and customer auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx';
 import { LanguageProvider } from './context/LanguageContext.jsx';
 import {CustomerAuthProvider} from './context/CustomerAuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
-      <AuthProvider>
         <CustomerAuthProvider>
           <App />
         </CustomerAuthProvider>
-      </AuthProvider>
     </LanguageProvider>
   </StrictMode>,
 );
